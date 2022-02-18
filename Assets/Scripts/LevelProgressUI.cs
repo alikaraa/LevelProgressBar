4c92ee10e0a6079a87c9147da90e0f22090e40e3
Ali Kara <56256760+alikaraa@users.noreply.github.com>
2022-02-18 16:11:55 +0300

    Level

---
+++ Assets/Scripts/LevelProgressUI.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine ;
+using UnityEngine.UI ;
+
+public class LevelProgressUI : MonoBehaviour {
+
+   [Header ("UI references :")]
+   [SerializeField] private Image uiFillImage ;
+   [SerializeField] private Text uiStartText ;
+   [SerializeField] private Text uiEndText ;
+
+   [Header ("Player & Endline references :")]
+   [SerializeField] private Transform playerTransform ;
+   [SerializeField] private Transform endLineTransform ;
+
+   
+   private Vector3 endLinePosition ;
+
+   private float fullDistance ;
+
+
+   private void Start () {
+      endLinePosition = endLineTransform.position ;
+      fullDistance = GetDistance () ;
+   }
+
+
+   public void SetLevelTexts (int level) {
+      uiStartText.text = level.ToString () ;
+      uiEndText.text = (level + 1).ToString () ;
+   }
+
+
+   private float GetDistance () {
+      return (endLinePosition - playerTransform.position).sqrMagnitude ;
+   }
+
+
+   private void UpdateProgressFill (float value) {
+      uiFillImage.fillAmount = value ;
+   }
+
+
+   private void Update () {
+   
+      if (playerTransform.position.z <= endLinePosition.z) {
+         float newDistance = GetDistance () ;
+         float progressValue = Mathf.InverseLerp (fullDistance, 0f, newDistance) ;
+
+         UpdateProgressFill (progressValue) ;
+      }
+   }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine ;
+
+public class Player : MonoBehaviour {
+
+   [SerializeField] private float speed = 10f ;
+
+
+   private void Update () {
+
+      float x = Input.GetAxis ("Horizontal") ;
+      float z = Input.GetAxis ("Vertical") ;
+
+      transform.position += new Vector3 (x, 0, z) * speed * Time.deltaTime ;
+
+   }
+
+
+}
